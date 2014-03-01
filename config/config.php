a2665020070009b6a6f04f98a811ef4039741f5f
Marko Cupic <m.cupic@gmx.ch>
2014-03-01 15:04:58 +0100

    01.03.2014 reformat code

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2005-2014 Leo Feyer
  *
  * @package IndependentAccordion
- * @author Marko Cupic <m.cupic@gmx.ch>
+ * @author  Marko Cupic <m.cupic@gmx.ch>
  * @link    https://contao.org
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
